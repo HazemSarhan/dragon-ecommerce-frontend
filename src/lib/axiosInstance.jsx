ced193b338323dc3d4d295dc47db8bc6780f62da
Hazem Megahed <hazoomy21@gmail.com>
2025-07-08 13:10:39 +0300

    Production Ready

--- src/lib/axiosInstance.jsx
+++ src/lib/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://dragon-ecommerce-backend-sx3y.onrender.com/api/v1',
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://dragon-ecommerce-backend-sx3y.onrender.com/api/v1',
+  // baseURL: 'http://localhost:3000/api/v1',
   withCredentials: true,
 });
 
